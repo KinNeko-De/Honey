75d1d9ea50733c57617711f14e98c3d07771c5ea
KinNeko <nils@diekmann-germany.de>
2019-11-03 16:03:26 +0100

    - added log file using serilog (massive performance decrease 500ms)

--- Honey/Program.cs
+++ Honey/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Honey.Commands;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+using MyApplicationExample;
+using Serilog;
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Honey
 {
@@ -13,7 +17,7 @@ namespace Honey
 			if (args == null) { throw new ArgumentNullException(nameof(args)); }
 
 			ProgramExitCode exitCode = ProgramExitCode.Ok;
-			ILogger logger = null;
+			Microsoft.Extensions.Logging.ILogger logger = null;
 
 			if (args == null)
 			{
@@ -48,18 +52,34 @@ namespace Honey
 			return (int)exitCode;
 		}
 
-		private static ILogger EnsureLoggerExists(ILogger logger)
+		private static Microsoft.Extensions.Logging.ILogger EnsureLoggerExists(Microsoft.Extensions.Logging.ILogger logger)
 		{
 			if(logger == null)
 			{
+				HoneyInstallLocation honeyInstallLocation = new HoneyInstallLocation();
+				string logFile = Path.Combine(honeyInstallLocation.GetInstallLocation(), "honey.log");
+
 				Stopwatch stopwatch = Stopwatch.StartNew();
+				Serilog.Log.Logger = new Serilog.LoggerConfiguration()
+					.MinimumLevel.Information() // because filtering over loggerfactory does not work
+					.MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning) // because filtering over loggerfactory does not work
+					.MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning) // because filtering over loggerfactory does not work
+					.MinimumLevel.Override("Honey", Serilog.Events.LogEventLevel.Debug) // because filtering over loggerfactory does not work
+					.Enrich.FromLogContext()
+					.WriteTo.File(logFile)
+					.CreateLogger();
+
 				var loggerFactory = LoggerFactory.Create(builder =>
 				{
 					builder
+						.SetMinimumLevel(LogLevel.Debug) // does not work for serilog
+						.AddSerilog(dispose: true)
 						.AddConsole()
-						.AddFilter("Microsoft", LogLevel.Warning)
-						.AddFilter("System", LogLevel.Warning)
-						.AddFilter("Honey", LogLevel.Debug);
+						.AddFilter("Microsoft", LogLevel.Warning) // does not work for serilog
+						.AddFilter("System", LogLevel.Warning) // does not work for serilog
+						.AddFilter("Honey", LogLevel.Debug) // does not work for serilog
+						.AddFilter<ConsoleLoggerProvider>("Honey", LogLevel.Information)
+						;
 				});
 				logger = loggerFactory.CreateLogger<Program>();
 				stopwatch.Stop();
